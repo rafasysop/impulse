0aeb0288b12c7e2bab8b16befb8d319f2542d296
Rafael Moura <rafaelcostamoura@gmail.com>
2022-04-26 09:15:51 -0300

    add user_id on service

--- src/controllers/CreateMessageController.ts
+++ src/controllers/CreateMessageController.ts
@@ -4,9 +4,10 @@ import { CreateMessageService } from '../services/CreateMessageService';
 class CreateMessageController {
   async handle(req: Request, res: Response) {
     const { message } = req.body
+    const { user_id } = req
     const service = new CreateMessageService();
     try {
-      const result = await service.execute(message)
+      const result = await service.execute(message, user_id)
       return res.json(result)
     } catch (error) {
       res.json({ error: error.message })
