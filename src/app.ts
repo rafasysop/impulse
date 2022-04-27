812992727f665b36eb5cc88d3a6213b035c77122
Rafael Moura <rafaelcostamoura@gmail.com>
2022-04-26 22:33:32 -0300

    configures Socket.Io

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,28 @@
 import 'dotenv/config'
 import express from 'express';
+import { Server } from 'socket.io'
+import http from 'http'
+import cors from 'cors'
 import { router } from './routes';
 
 const port = 4000
 
 const app = express();
+app.use(cors())
+
+const serverHttp = http.createServer(app)
+
+const io = new Server(serverHttp, {
+  cors: {
+    origin: '*'
+  }
+})
+
+io.on('connection', socket => {
+  console.log('usuario conectado no socket', socket.id);
+  
+})
+
 app.use(express.json())
 app.use(router);
 
